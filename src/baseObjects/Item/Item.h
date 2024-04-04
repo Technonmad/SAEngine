8b25fee740ce1218a9d1261851905f94f98b5161
Pavlukhin Anton <65555937+Technonmad@users.noreply.github.com>
2024-04-04 22:46:36 +0300

    add: polygon() method to the Item class

--- src/baseObjects/Item/Item.h
+++ src/baseObjects/Item/Item.h
@@ -2,7 +2,10 @@
 #define ITEM_H
 
 #include <QGraphicsItem>
+#include <QGraphicsScene>
 #include <QMenu>
+
+class Arrow;
 
 class Item: public QGraphicsItem
 {
@@ -15,16 +18,18 @@ public:
     void removeArrow(Arrow *arrow);
     void removeArrows();
     DiagramType diagramType() const;
+    QPolygonF polygon() const;
     void addArrow(Arrow *arrow);
     QPixmap image() const;
     int type() const override;
 
 protected:
     void contextMenuEvent(QGraphicsSceneContextMenuEvent *event) override;
-    QVariant itemChange(GraphicsItemChange change, const QVariant &value) oevrride;
+    QVariant itemChange(GraphicsItemChange change, const QVariant &value) override;
 
 private:
     DiagramType myDiagramType;
+    QPolygonF myPolygon;
     QMenu *myContextMenu;
     QList<Arrow *> arrows;
 };
