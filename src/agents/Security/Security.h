e5d288952d44624af1cfe9066f7d5e5869827c42
Pavlukhin Anton <65555937+Technonmad@users.noreply.github.com>
2024-05-04 16:02:06 +0300

    add: Security class

---
+++ src/agents/Security/Security.h
@@ -0,0 +1,39 @@
+#ifndef SECURITY_H
+#define SECURITY_H
+
+#include "Arrow/Arrow.h"
+#include <QTextEdit>
+#include <QVariant>
+#include <Item/GraphicsItem.h>
+
+class Security : public GraphicsItem
+{
+    Q_OBJECT
+public:
+    Security(QMenu *contextMenu, QGraphicsItem *parent = nullptr);
+
+    ~Security();
+
+//public:
+//    QGraphicsPixmapItem *m_pixmapItem;
+//    QGraphicsTextItem *m_textItem;
+
+private:
+    QRectF boundingRect() const override;
+    void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget) override;
+    void addArrow(Arrow *arrow) override;
+    int type() const override;
+    QVariant itemChange(GraphicsItemChange change, const QVariant &value) override;
+    void removeArrow(Arrow *arrow) override;
+    void removeArrows() override;
+
+signals:
+    void sendMessage(const QString &message);
+
+public slots:
+    void receiveMessage(const QString &message) override;
+private:
+//    QList<Arrow *> arrows;
+};
+
+#endif // SECURITY_H
