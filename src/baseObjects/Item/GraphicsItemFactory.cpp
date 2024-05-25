35167d01487b5fa3588f1c82cc4b3a3089f8b7b6
Pavlukhin Anton <65555937+Technonmad@users.noreply.github.com>
2024-05-25 23:23:35 +0300

    refactor: (continue) removed several agents

--- src/baseObjects/Item/GraphicsItemFactory.cpp
+++ src/baseObjects/Item/GraphicsItemFactory.cpp
@@ -1,15 +1,10 @@
 #include "GraphicsItemFactory.h"
 #include <Storing/WarehouseItem.h>
-#include <AccessControl/AccessControl.h>
-#include <Cam/Cam.h>
-#include <Engineers/Engineers.h>
 #include <Firefighters/Firefighters.h>
 #include <FireSensor/FireSensor.h>
 #include <Managers/Managers.h>
-#include <Security/Security.h>
 #include <Tecnician/Tecnician.h>
 #include <Delivery/Delivery.h>
-#include <Packaging/Packaging.h>
 #include <Production/Production.h>
 
 GraphicsItemFactory::GraphicsItemFactory()
@@ -25,15 +20,6 @@ GraphicsItemFactory::~GraphicsItemFactory()
 GraphicsItem *GraphicsItemFactory::create(GraphicsItem::DiagramType diagramType, QMenu *contextMenu, QGraphicsItem *parent)
 {
     switch (diagramType) {
-    case GraphicsItem::AccessControl:
-        return new AccessControl(contextMenu, parent);
-        break;
-    case GraphicsItem::AICamera:
-        return new Cam(contextMenu, parent);
-        break;
-    case GraphicsItem::Engineers:
-        return new Engineers(contextMenu, parent);
-        break;
     case GraphicsItem::Firefighters:
         return new Firefighters(contextMenu, parent);
         break;
@@ -43,17 +29,11 @@ GraphicsItem *GraphicsItemFactory::create(GraphicsItem::DiagramType diagramType,
     case GraphicsItem::Managers:
         return new Managers(contextMenu, parent);
         break;
-    case GraphicsItem::SecurityPost:
-        return new Security(contextMenu, parent);
-        break;
     case GraphicsItem::Tecnician:
         return new Tecnician(contextMenu, parent);
         break;
     case GraphicsItem::Delivery:
         return new Delivery(contextMenu, parent);
-        break;
-    case GraphicsItem::PackingLine:
-        return new Packaging(contextMenu, parent);
         break;
     case GraphicsItem::ProductionLine:
         return new Production(contextMenu, parent);
